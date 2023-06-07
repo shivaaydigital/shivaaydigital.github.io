61d0fbbe1bb675207ef9332666fb7a60b1df3807
Harshit Raheja <quantumbytestudios@gmail.com>
2023-06-08 01:10:50 +0530

    Update

---
+++ contact_scripts/contact.php
@@ -0,0 +1,33 @@
+<?php
+
+// Get Message Details
+$alert = "NEW MESSAGE";
+$name =  "Name: " . $_POST[''];
+$email =  "Email: " . $_POST[''];
+$message =  "Message: " . $_POST[''];
+
+// Test Message Details
+// $alert = "NEW MESSAGE";
+// $name =  "Name: " . "Crytomag";
+// $email =  "Email: " . "Test";
+// $message =  "Message: " . "Test";
+
+
+$final_message = $alert . "\n" . $name . "\n" . $email . "\n" . $message;
+
+$url = "https://api.telegram.org/bot5897503467:AAEH3z4XUP2KmDjnxpVh8o-QN69WIIsdCJg/sendMessage?chat_id=6137503985&text=" . urlencode($final_message);
+// echo $url;
+
+// Send Request
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+curl_exec($ch);
+curl_close($ch);
+
+
+echo "<script>window.location.href = 'https://quantumbyte.studio/contact/';</script>";
